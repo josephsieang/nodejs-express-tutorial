cc19e007623f8d04476dbdef5823f597d4fae3ff
Joseph Ng <joseph_sieang@live.com>
2022-01-13 10:10:14 +0800

    Add alternative syntax to export modules

--- nodejs/3-modules.js
+++ nodejs/3-modules.js
@@ -2,6 +2,8 @@
 // Modules - Encapsulated Code (only share minimum)
 const names = require('./4-name');
 const sayHi = require('./5-utils');
+const data = require('./6-alternative-syntax');
 
 // console.log(names);
 sayHi(names.john);
+console.log(data);
