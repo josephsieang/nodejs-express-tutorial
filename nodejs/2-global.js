b32e44364d4a68c4573d1685fe556b438343a753
Joseph Ng <joseph_sieang@live.com>
2022-01-13 09:49:49 +0800

    Add 2-global.js

---
+++ nodejs/2-global.js
@@ -0,0 +1,10 @@
+// GLOBALS - No Window
+
+// __dirname - path to current directory
+// __filename - __filename
+// require - function to use modules (commonJS)
+// module - info about current module (file)
+// process - info about env where the program is being executed
+
+console.log(__dirname);
+setTimeout(() => console.log('hello'), 1000);
